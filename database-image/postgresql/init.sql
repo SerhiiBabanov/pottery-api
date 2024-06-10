1e9c3622def3257d6d2fdac923b8e94237c080c8
Serhii Babanov <serhii.babanov@gmail.com>
2024-06-10 18:14:13 -0400

    init commit

---
+++ database-image/postgresql/init.sql
@@ -0,0 +1,3 @@
+CREATE ROLE service_products WITH LOGIN PASSWORD 'srv-pds';
+
+CREATE SCHEMA service_products AUTHORIZATION service_products;
---
+++ service-products/src/main/java/com/pottery/service/products/ServiceProductsApplication.java
@@ -0,0 +1,15 @@
+package com.pottery.service.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceProductsApplication.class, args);
+	}
+
+}
